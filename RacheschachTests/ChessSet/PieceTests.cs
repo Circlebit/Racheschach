83b4fae4db0e7246627110103735c0e802b52278
Paul Seibel <paul.seibel@mailbox.org>
2020-08-30 17:53:10 +0200

    more tests for GetPossibleMovesForBishop

--- RacheschachTests/ChessSet/PieceTests.cs
+++ RacheschachTests/ChessSet/PieceTests.cs
@@ -83,38 +83,30 @@ namespace Racheschach.ChessSet.Tests
                 b.GetNewMove("b2","a1")};
             Assert.Equal(whiteBishopLAssertMoves, whiteBishopL.GetPossibleMoves());
 
-            //TODO:test the rest
+            Piece whiteBishopR = b.GetSquareBySquareNotation("f1").Piece;
+            var whiteBishopRAssertMoves = new List<Move>() {
+                b.GetNewMove("f1","e2"),
+                b.GetNewMove("f1","d3"),
+                b.GetNewMove("f1","g2")};
+            Assert.Equal(whiteBishopRAssertMoves, whiteBishopR.GetPossibleMoves());
 
-            //Piece whiteBishopR = b.GetSquareBySquareNotation("h3").Piece;
-            //var whiteBishopRAssertMoves = new List<Move>() {
-            //    b.GetNewMove("h3","h2"),
-            //    b.GetNewMove("h3","h1"),
-            //    b.GetNewMove("h3","g3"),
-            //    b.GetNewMove("h3","f3"),
-            //    b.GetNewMove("h3","e3"),
-            //    b.GetNewMove("h3","d3"),
-            //    b.GetNewMove("h3","c3"),
-            //    b.GetNewMove("h3","b3"),
-            //    b.GetNewMove("h3","a3")};
-            //Assert.Equal(whiteBishopRAssertMoves, whiteBishopR.GetPossibleMoves());
+            Piece blackBishopL = b.GetSquareBySquareNotation("c8").Piece;
+            var blackBishopLAssertMoves = new List<Move>() {
+                b.GetNewMove("c8","d7"),
+                b.GetNewMove("c8","e6"),
+                b.GetNewMove("c8","b7"),
+                b.GetNewMove("c8","a6")};
+            Assert.Equal(blackBishopLAssertMoves, blackBishopL.GetPossibleMoves());
 
-            //Piece blackBishopL = b.GetSquareBySquareNotation("a8").Piece;
-            //var blackBishopLAssertMoves = new List<Move>() {
-            //    b.GetNewMove("a8","a7"),
-            //    b.GetNewMove("a8","a6")};
-            //Assert.Equal(blackBishopLAssertMoves, blackBishopL.GetPossibleMoves());
-
-            //Piece blackBishopR = b.GetSquareBySquareNotation("f5").Piece;
-            //var blackBishopRAssertMoves = new List<Move>() {
-            //    b.GetNewMove("f5","f6"),
-            //    b.GetNewMove("f5","f7"),
-            //    b.GetNewMove("f5","g5"),
-            //    b.GetNewMove("f5","h5"),
-            //    b.GetNewMove("f5","f4"),
-            //    b.GetNewMove("f5","f3"),
-            //    b.GetNewMove("f5","f2"),
-            //    b.GetNewMove("f5","e5")};
-            //Assert.Equal(blackBishopRAssertMoves, blackBishopR.GetPossibleMoves());
+            Piece blackBishopR = b.GetSquareBySquareNotation("a3").Piece;
+            var blackBishopRAssertMoves = new List<Move>() {
+                b.GetNewMove("a3","b4"),
+                b.GetNewMove("a3","c5"),
+                b.GetNewMove("a3","d6"),
+                b.GetNewMove("a3","e7"),
+                b.GetNewMove("a3","f8"),
+                b.GetNewMove("a3","b2")};
+            Assert.Equal(blackBishopRAssertMoves, blackBishopR.GetPossibleMoves());
 
 
             var b2 = new Board();
